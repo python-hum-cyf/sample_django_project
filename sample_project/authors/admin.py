6bccbff7c385f1f0d674e7fa505678517d3c9655
python-hum-cyf <117757538+python-hum-cyf@users.noreply.github.com>
2023-06-13 18:15:06 +0200

    Update admin.py with new admin method

--- sample_project/authors/admin.py
+++ sample_project/authors/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Author
 
 # Register your models here.
-admin.site.register(Author)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ('name', 'year_of_birth', 'year_of_death', 'country_of_origin', 'years_of_life')
+    list_display = ('name', 'year_of_birth', 'year_of_death')
+    readonly_fields = ['years_of_life']
+
+    def years_of_life(self, obj):
+        if obj.year_of_birth is None or obj.year_of_death is None:
+            return 'Unknown'
+        return obj.year_of_death - obj.year_of_birth
+
+
